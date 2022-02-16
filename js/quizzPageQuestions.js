f0f1f52a2e04f0a2dc8eb809d03820f274f904a6
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-02-16 22:49:43 +0000

    Selecting answer

--- js/quizzPageQuestions.js
+++ js/quizzPageQuestions.js
@@ -1,10 +1,31 @@
+let containerAnswers = [];
+
+for(let i = 0; i < 2; i++){ // tem que ser a quantidade de perguntas
+    containerAnswers.push(true);
+}
 
 // Função selecionar resposta
-function selectAnswer(div){
+function selectAnswer(div,num){
+
+    console.log(containerAnswers)
+    if(containerAnswers[num-1]){
+
+        containerAnswers[num-1] = false;
+
+        let numAnswer = parseFloat(div.id.replace('answer', '')); 
+        
+        let myElement = document.querySelector(`.blockQuizzT2__${num}`);
+
+        for (let i = 0; i < myElement.children.length + 2; i++){
+            console.log(i);
+            if(i != (numAnswer-1)){
+                let otherAnswers = document.getElementById(`block${num}_answer${i+1}`);
+                otherAnswers.classList.add("notSelected");
+            }
+        
+        }
+    }
 
     // fazer comparador com a resposta que vem do axios-- para saber se é true ou false a resposta
-    
-    // add classe a div selecionada
-    div.classList.add("selected");
-
+    console.log(num);
 }
