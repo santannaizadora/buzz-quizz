301433ee5055f35a210a2719cd4c5d60e744ad90
santannaizadora <santannaizadora@gmail.com>
2022-02-16 16:00:28 -0300

    validate basic infos

--- js/createQuizz.js
+++ js/createQuizz.js
@@ -24,6 +24,9 @@ let quizzObj = {
     ]
 }
 
+let numQuestions = 0
+let numLevels = 0
+
 const isQuizzTitleValid = (title) => {
     if (title.length >= 20 && title.length <= 65) {
         return true
@@ -48,4 +51,73 @@ const isNumLevelsValid = (numLevels) => {
         return true
     }
     return false
+}
+
+const getBasicInfos = () => {
+    quizzObj.title = document.getElementById('create-title').value
+    quizzObj.image = document.getElementById('create-img-url').value
+    numQuestions = document.getElementById('create-num-questions').value
+    numLevels = document.getElementById('create-num-levels').value
+}
+
+const validateBasicInfos = () => {
+    getBasicInfos()
+    if (isQuizzTitleValid(quizzObj.title) && isValidUrl(quizzObj.image) && isNumQuestionsValid(numQuestions) && isNumLevelsValid(numLevels)) {
+        return true
+    }
+    return false
+}
+
+const invalidBasicInfos = () => {
+    let invalidTitleMessage = document.getElementById('ivalid-quizz-tile')
+    let invalidTitleInput = document.getElementById('create-title')
+    if (!isQuizzTitleValid(quizzObj.title)) {
+        invalidTitleMessage.classList.remove('hidden')
+        invalidTitleInput.classList.add('invalid-input')
+    } else {
+        invalidTitleMessage.classList.add('hidden')
+        invalidTitleInput.classList.remove('invalid-input')
+    }
+
+    let invalidUrlMessage = document.getElementById('ivalid-img-url')
+    let invalidUrlInput = document.getElementById('create-img-url')
+    if (!isValidUrl(quizzObj.image)) {
+        invalidUrlMessage.classList.remove('hidden')
+        invalidUrlInput.classList.add('invalid-input')
+    } else {
+        invalidUrlMessage.classList.add('hidden')
+        invalidUrlInput.classList.remove('invalid-input')
+    }
+
+    let invalidQuestionsMessage = document.getElementById('invalid-num-questions')
+    let invalidQuestionsInput = document.getElementById('create-num-questions')
+
+    if (!isNumQuestionsValid(numQuestions)) {
+        invalidQuestionsMessage.classList.remove('hidden')
+        invalidQuestionsInput.classList.add('invalid-input')
+    } else {
+        invalidQuestionsMessage.classList.add('hidden')
+        invalidQuestionsInput.classList.remove('invalid-input')
+    }
+
+    let invalidLevelsMessage = document.getElementById('invalid-num-levels')
+    let invalidLevelsInput = document.getElementById('create-num-levels')
+
+    if (!isNumLevelsValid(numLevels)) {
+        invalidLevelsMessage.classList.remove('hidden')
+        invalidLevelsInput.classList.add('invalid-input')
+    } else {
+        invalidLevelsMessage.classList.add('hidden')
+        invalidLevelsInput.classList.remove('invalid-input')
+    }
+}
+
+const goToQuestions = () => {
+    let isAllValid = validateBasicInfos()
+    if (isAllValid) {
+        document.querySelector('.create-quizz-info').classList.add('hidden')
+        document.querySelector('.create-quizz-questions').classList.remove('hidden')
+    } else {
+        invalidBasicInfos()
+    }
 }
