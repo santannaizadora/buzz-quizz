ba5e1ddb3eb1a8702af39512990f110439b8f1a2
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-02-16 13:14:16 +0000

    CSS layout of screen 2

---
+++ js/quizzPageQuestions.js
@@ -0,0 +1,10 @@
+
+// Função selecionar resposta
+function selectAnswer(div){
+
+    // fazer comparador com a resposta que vem do axios-- para saber se é true ou false a resposta
+    
+    // add classe a div selecionada
+    div.classList.add("selected");
+
+}
