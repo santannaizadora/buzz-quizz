7c0f2cf25af8dd66f839df4e03c772ba4c8f56a8
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-02-17 10:32:52 +0000

    feat: Add result info css

--- js/quizzPageQuestions.js
+++ js/quizzPageQuestions.js
@@ -7,7 +7,7 @@ for(let i = 0; i < 2; i++){ // tem que ser a quantidade de perguntas
 // Função selecionar resposta
 function selectAnswer(div,num){
 
-    console.log(containerAnswers)
+    /* console.log(containerAnswers) */
     if(containerAnswers[num-1]){
 
         containerAnswers[num-1] = false;
@@ -16,16 +16,27 @@ function selectAnswer(div,num){
         
         let myElement = document.querySelector(`.blockQuizzT2__${num}`);
 
-        for (let i = 0; i < myElement.children.length + 2; i++){
-            console.log(i);
+        for (let i = 0; i < myElement.children.length*2; i++){
+            /* console.log(i); */
             if(i != (numAnswer-1)){
                 let otherAnswers = document.getElementById(`block${num}_answer${i+1}`);
                 otherAnswers.classList.add("notSelected");
             }
         
         }
+
+        /* myElement.children.next.scrollIntoView({behavior: "smooth"}); */
     }
 
+    
     // fazer comparador com a resposta que vem do axios-- para saber se é true ou false a resposta
-    console.log(num);
 }
+
+/* setInterval(selectAnswer, 2000); */
+
+/* function scrollToBottom(number) {
+    console.log(number);
+    let myElement = document.querySelector(`.blockQuizzT2__${number}`);
+    myElement.scrollIntoView(false); // Bottom
+} */
+
